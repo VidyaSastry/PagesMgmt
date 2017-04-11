12dffe54db82aa2666bc5b89ea265d44f3016a72
Sree Vidya Sastry <sreevidya.sastry12@gmail.com>
2017-04-10 18:08:26 -0700

    Initial commit

---
+++ app/src/main/java/com/vidya/pagesmgmt/controllers/requests/AccessTokenGenerator.java
@@ -0,0 +1,8 @@
+package com.vidya.pagesmgmt.controllers.requests;
+
+/**
+ * Created by Vidya on 4/10/17.
+ */
+
+public class AccessTokenGenerator {
+}
---
+++ app/src/main/java/com/vidya/pagesmgmt/screens/LoginActivity.java
@@ -0,0 +1,95 @@
+package com.vidya.pagesmgmt.screens;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.vidya.pagesmgmt.R;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final String[] perms = {"manage_pages","publish_actions"};
+    private  static final List<String> PERM = Arrays.asList(perms);
+
+    protected LoginButton loginButton;
+
+    CallbackManager callbackManager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        callbackManager = CallbackManager.Factory.create();
+        setContentView(R.layout.activity_login);
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+
+        if(accessToken!=null){
+            Log.i("Access",accessToken.toString());
+            Intent intent = new Intent(this,HomeActivity.class);
+            startActivity(intent);
+        }
+
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Intent intent = new Intent(LoginActivity.this,HomeActivity.class);
+                startActivity(intent);
+                Log.i("AcessToken",loginResult.getAccessToken().toString());
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(getApplicationContext(),R.string.login_cancelled,Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Log.i("error",exception.getMessage());
+                Toast.makeText(getApplicationContext(),R.string.login_failed,Toast.LENGTH_LONG).show();
+            }
+        });
+
+        if(loginButton.getFragment()!=null){
+            LoginManager.getInstance().logInWithPublishPermissions(loginButton.getFragment(),PERM);
+        }else{
+            LoginManager.getInstance().logInWithPublishPermissions(LoginActivity.this,PERM);
+        }
+
+//        try {
+//            PackageInfo info = getPackageManager().getPackageInfo(
+//                    "com.example.android.pagesmanagementapp",
+//                    PackageManager.GET_SIGNATURES);
+//            for (Signature signature : info.signatures) {
+//                MessageDigest md = MessageDigest.getInstance("SHA");
+//                md.update(signature.toByteArray());
+//                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+//            }
+//        } catch (PackageManager.NameNotFoundException e) {
+//
+//        } catch (NoSuchAlgorithmException e) {
+//
+//        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+}
